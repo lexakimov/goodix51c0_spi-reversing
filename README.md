e750bcdbe49daca9706d589d145fb722fe30d7a8
Alexey Akimov <lex.akimov23@gmail.com>
2024-01-14 15:14:23 +0300

    add russian README.md

--- README.md
+++ README.md
@@ -1,2 +1,104 @@
-# goodix51c0_spi-protocol-reversing
-Goodix 51C0 SPI fingerprint scanner interaction protocol reverse engineering repository
+# Реверс-инжиниринг драйвера сканера отпечатка пальцев Goodix 51C0 (SPI)
+
+## Настройка драйвера `spidev`
+
+Сначала проверьте что устройство действительно отображается в sysfs:
+```shell
+ls /sys/bus/spi/devices/
+# > spi0.0  spi-GDIX51C0:00
+```
+Если устройство `spi-GDIX51C0:00` присутствует, выполните шаг за шагом:
+```shell
+sudo su
+modprobe spidev
+
+# проверить, что spidev загружен:
+lsmod | grep spidev
+# > spidev                 28672  0
+
+# подсоединение устройства spi-GDIX51C0:00 к драйверу spidev
+# взято отсюда https://docs.kernel.org/spi/spidev.html
+echo spidev > /sys/bus/spi/devices/spi-GDIX51C0:00/driver_override
+echo spi-GDIX51C0:00 > /sys/bus/spi/drivers/spidev/bind
+
+# проверить, что устройство spidev появилось в /dev:
+ls /dev/spidev*
+# > /dev/spidev1.0
+```
+
+## Настройка виртуального окружения
+Прежде чем выполнять шаги, необходимо чтобы у вас был установлен Python 3 с установленным `virtualenv`.
+
+В корневой папке проекта создайте виртуальное окружение:
+```shell
+virtualenv venv
+```
+
+Активируйте его
+```shell
+source ./venv/bin/activate
+```
+
+Установите зависимости
+```shell
+pip install -r requirements.txt
+```
+
+Для того чтобы у python был доступ к SPI и GPIO устройствам, его нужно запускать из под root-пользователя.
+
+Чтобы не запрашивать пароль каждый раз или при запуске из IDE, создайте в виртуальном окружении скрипт для запуска
+интерпретатора python из под root-пользователя:
+```shell
+cat << EOF > ./venv/bin/python-sudo
+#!/bin/bash
+sudo $(pwd)/venv/bin/python "\$@"
+EOF
+chmod +x ./venv/bin/python-sudo
+```
+
+Чтобы не было запроса пароля, создайте файл `/etc/sudoers.d/venv-python` со следующим содержанием:
+```
+user_name machine_name = (root) NOPASSWD: /path/to/project/venv/bin/python
+```
+Например, так:
+```
+akimov huawei-rlefxx = (root) NOPASSWD: /home/akimov/desktop/gdix51c0-spi-reversing/venv/bin/python
+```
+
+Теперь в виртуальном окружении можно запускать python из под root пользователя без запроса пароля: 
+вместо `python` используйте команду `python-sudo`.
+
+Проверка, что все было настроено правильно:
+```
+(venv) [akimov@huawei-rlefxx gdix51c0-spi-reversing]$ python-sudo 
+Python 3.11.6 (main, Nov 14 2023, 09:36:21) [GCC 13.2.1 20230801] on linux
+Type "help", "copyright", "credits" or "license" for more information.
+>>> import getpass
+>>> getpass.getuser()
+'root'
+>>> exit()
+```
+
+## Запуск скрипта
+
+```shell
+sudo python protocol_interaction.py
+# or
+python-sudo protocol_interaction.py
+```
+
+## Отладка в IDE
+Для отладки можно запускать скрипт в среде разработки (например PyCharm или Intellij IDEA с установленным плагинов Python)
+
+### Настройка
+- в Intellij IDEA зайдите в меню `File -> Project Structure`, затем в `SDKs -> Add new SDK -> Add Python SDK`
+- на вкладке `Virtual environment -> Existing environment -> Interpreter ...` и выберите файл `[project path]/venv/bin/python-sudo`
+Все будет выглядеть примерно так:
+<img src="images/ide-interpreter-setup.png" alt="images/ide-interpreter-setup.png">
+- нажмите `Apply`
+- перейдите на вкладку `Project`
+- выберите в выпадающем списке SDK созданный интерпретатор
+- нажмите OK
+
+### Отладка
+Запуск отладки через клик ПКМ на [protocol_interaction.py](protocol_interaction.py) -> `Debug 'protocol_interaction.py'`.
---
+++ protocol_interaction.py
@@ -0,0 +1,133 @@
+import asyncio
+
+import periphery
+from periphery import CdevGPIO
+from spidev import SpiDev
+
+
+class Log:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+    @staticmethod
+    def green(message):
+        print(Log.OKGREEN, message, Log.ENDC, sep='')
+
+    @staticmethod
+    def cyan(message):
+        print(Log.OKCYAN, message, Log.ENDC, sep='')
+
+    @staticmethod
+    def red(message):
+        print(Log.FAIL, message, Log.ENDC, sep='')
+
+    @staticmethod
+    def white(message):
+        print(Log.HEADER, message, Log.ENDC, sep='')
+
+
+def format_to_hex_string(byte_array: bytearray | bytes):
+    return ' '.join('{:02X}'.format(num) for num in byte_array)
+
+
+def format_to_utf_string(byte_array: bytearray | bytes):
+    return bytearray(byte_array).decode('utf-8', errors='ignore')
+
+
+def make_cmd_packet(cmd: int, payload: bytes | list) -> bytearray:
+    """
+    cmd
+        command 0xA0
+    payload
+        bytes of payload
+    """
+    payload_length: bytes = len(payload).to_bytes(2, 'little')
+    command_packet = bytearray([cmd]) + payload_length
+    checksum: bytes = sum(command_packet).to_bytes()
+    command_packet += checksum
+
+    return command_packet
+
+
+def reset_spi():
+    Log.red("reset...")
+    gpio_reset = CdevGPIO('/dev/gpiochip0', 140, 'out', label='fp-reset')
+    for i in (1, 0):
+        gpio_reset.write(bool(i))
+    gpio_reset.close()
+
+
+async def interrupt_monitoring(gpio_line: periphery.CdevGPIO, ready_to_write_lock: asyncio.Lock):
+    Log.cyan("interrupt_monitoring started")
+    ready_to_write_lock.release()
+    i = 1
+    while True:
+        await asyncio.sleep(0.001)
+        r = gpio_line.read()
+        if r:
+            Log.cyan(f"release read lock on iteration {i}")
+            return True
+        i += 1
+        if i > 5000:
+            Log.red(f"timeout {i}")
+            return False
+
+
+async def perform_tx(spi: SpiDev, interrupt_line: CdevGPIO, cmd: int, payload: bytes):
+    ready_to_write_lock = asyncio.Lock()
+    await ready_to_write_lock.acquire()
+    print("latch created")
+
+    ir_monitoring = asyncio.create_task(interrupt_monitoring(interrupt_line, ready_to_write_lock))
+    await ready_to_write_lock.acquire()
+
+    print("start writing bytes")
+
+    cmd_packet = make_cmd_packet(cmd, payload)
+    spi.writebytes(cmd_packet)
+    Log.white(f"sent command: {format_to_hex_string(cmd_packet)} | {format_to_utf_string(cmd_packet)}")
+    spi.writebytes(payload)
+    Log.white(f"sent payload : {format_to_hex_string(payload)} | {format_to_utf_string(payload)}")
+
+    print("waiting for gpio interrupt...")
+    await ir_monitoring
+    print("lock released, execution goes on")
+
+    response_1 = spi.readbytes(4)
+    Log.white(f"received package: {format_to_hex_string(response_1)} | {format_to_utf_string(response_1)}")
+    # TODO проверка контрольных сумм
+    # TODO парсить длину ответа
+    response_2 = spi.readbytes(6)
+    Log.white(f"received package: {format_to_hex_string(response_2)} | {format_to_utf_string(response_2)}")
+
+
+async def main():
+    gpio_line = CdevGPIO('/dev/gpiochip0', 321, 'in', edge='both', bias='default')
+    spi = SpiDev(1, 0)
+    spi.max_speed_hz = 0x00989680  # 10 000 000
+    # spi.mode = 0b00
+
+    # reset_spi()
+
+    await perform_tx(spi, gpio_line, 0xa0, bytearray([0xe4, 0x09, 0x00, 0x02, 0x00, 0x01, 0xbb, 0x00, 0x00, 0x00, 0x00, 0xff]))
+    # read    4 -  0000: a0 06 00 a6
+    # read    6 -  0000: b0 03 00 e4 07 0c
+
+    await perform_tx(spi, gpio_line, 0xa0, bytearray([0xa8, 0x03, 0x00, 0x00, 0x00, 0xff]))
+    # read    4 -  0000: a0 1a 00 ba
+    # read   26 -  0000: a8 17 00 47 46 5f 48 43 34 36 30 53 45 43 5f 41
+    # read   26 -  0010: 50 50 5f 31 34 32 31 30 00 68
+
+    spi.close()
+    gpio_line.close()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
