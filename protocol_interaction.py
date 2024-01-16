8e185f66aa5807f2fffbf41039c8314b2c925810
Alexey Akimov <lex.akimov23@gmail.com>
2024-01-16 22:26:17 +0300

    extract util module

--- protocol_interaction.py
+++ protocol_interaction.py
@@ -4,52 +4,7 @@ import periphery
 from periphery import CdevGPIO
 from spidev import SpiDev
 
-
-class Colors:
-    END = "\033[0m"
-
-    """ ANSI color codes """
-    BLACK = "\033[0;30m"
-    RED = "\033[0;31m"
-    GREEN = "\033[0;32m"
-    BROWN = "\033[0;33m"
-    BLUE = "\033[0;34m"
-    PURPLE = "\033[0;35m"
-    CYAN = "\033[0;36m"
-    LIGHT_GRAY = "\033[0;37m"
-    DARK_GRAY = "\033[1;30m"
-    LIGHT_RED = "\033[1;31m"
-    LIGHT_GREEN = "\033[1;32m"
-    YELLOW = "\033[1;33m"
-    LIGHT_BLUE = "\033[1;34m"
-    LIGHT_PURPLE = "\033[1;35m"
-    LIGHT_CYAN = "\033[1;36m"
-    LIGHT_WHITE = "\033[1;37m"
-
-    BOLD = "\033[1m"
-    FAINT = "\033[2m"
-    ITALIC = "\033[3m"
-    UNDERLINE = "\033[4m"
-    BLINK = "\033[5m"
-    NEGATIVE = "\033[7m"
-    CROSSED = "\033[9m"
-
-
-def log(color, message):
-    print(color, message, Colors.END, sep='')
-
-
-def to_hex_string(byte_array: bytearray | bytes):
-    return ' '.join('{:02X}'.format(num) for num in byte_array)
-
-
-def to_utf_string(byte_array: bytearray | bytes):
-    length = extract_length(byte_array)
-    return f'[length: {length}] ' + bytearray(byte_array[3:]).decode('utf-8', errors='ignore')
-
-
-def format_validity(is_valid: bool) -> str:
-    return '🟢' if is_valid else '🔴'
+from util_fmt import Colors, log, to_hex_string, to_utf_string, format_validity
 
 
 def make_protocol_packet(packet_type: int, payload_length: int | list) -> bytearray:
